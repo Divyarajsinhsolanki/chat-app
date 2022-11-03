ea7f9546c32a844b94c64133a084a7742b47e97b
Divyarajsinh <divyaraj@atharvasystem.com>
2022-11-03 11:31:57 +0530

    db

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Chat
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    config.hosts << fast-wave-25195.herokuapp.com
+    config.hosts << "fast-wave-25195.herokuapp.com"
 
     # Configuration for the application, engines, and railties goes here.
     #
